8ddc83ad94c1f448bec60ac9ecea5f1172a4e6d3
0xTunay <tigrus232323k@gmail.com>
2025-12-03 18:39:29 +0300

    init commit

---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+#include "freertos/FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "freertos/task.h"
+
+static const char * TAG = "Module";
+
+void app_main(void)
+{
+    
+}
